c1ab60b429f39e3558e41aa1a2ca091d73228e3b
sunil rana <sunilrana1730@gmail.com>
2022-07-29 02:11:01 +0530

    description and filter functionality

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var form  = document.getElementById('addform')
 var itemList = document.getElementById('items');
+var filter  = document.getElementById('filter');
+
 // Form submit event
 form.addEventListener('submit',addItem);
 itemList.addEventListener('click',removeItem);
+filter.addEventListener('keyup',filterItems);
 
 function addItem(e){
   e.preventDefault();
@@ -12,16 +15,17 @@ function addItem(e){
   li.className = 'list-group-item';
   
   li.appendChild(document.createTextNode(newItem));
-  var deleteBtn = document.createElement('button');
-  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-  deleteBtn.appendChild(document.createTextNode('X'));
-  li.appendChild(deleteBtn);
-  itemList.appendChild(li);
   var editbutton  = document.createElement('button');
   editbutton.className  = 'btn btn-sm float-right edit';
   editbutton.appendChild(document.createTextNode('Edit'));
   li.appendChild(editbutton);
   itemList.appendChild(li);
+  var deleteBtn = document.createElement('button');
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  deleteBtn.appendChild(document.createTextNode('X'));
+  li.appendChild(deleteBtn);
+  itemList.appendChild(li);
+  
 }
 // Remove item
 function removeItem(e){
@@ -29,4 +33,18 @@ function removeItem(e){
       var li = e.target.parentElement;
       itemList.removeChild(li);
     }
+  }
+
+  //  filter  
+  function filterItems(e){
+    var text = e.target.value.toLowerCase();
+    var items = itemList.getElementsByTagName('li');
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
   }
