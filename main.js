d777e7602f1f87685f27f571b788145dbe22ce4f
sunil rana <sunilrana1730@gmail.com>
2022-07-29 01:24:45 +0530

    updating main.js for delete button

--- main.js
+++ main.js
@@ -1,52 +1,27 @@
-// ELEMENT SELECTORS
+var form  = document.getElementById('addform')
+var itemList = document.getElementById('items');
+// Form submit event
+form.addEventListener('submit',addItem);
+itemList.addEventListener('click',removeItem);
 
-// Single Element Selectors
-console.log(document.getElementById('my-form'));
-console.log(document.querySelector('.container'));
-const ul = document.querySelector('.items');
-// ul.remove();
-// ul.lastElementChild.remove();
-
-//ul.children[1].innerText = 'sunil';
-document.getElementById('my-form').style.background = 'lightblue'
-document.getElementById("myP").style.color = "green";
-document.getElementById("myP2").style.color = "yellow";
-document.getElementById("myDiv").style.color = "red";
-
-  const myForm = document.querySelector('#my-form');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
-const msg = document.querySelector('.msg');
-const userList = document.querySelector('#users');
-
-// Listen for form submit
-myForm.addEventListener('submit', onSubmit);
-
-function onSubmit(e) {
+function addItem(e){
   e.preventDefault();
+  var newItem = document.getElementById('item').value;
+  var li = document.createElement('li');
   
-  if(nameInput.value === '' || emailInput.value === '') {
-    //alert('Please enter all fields');
-    msg.classList.add('error');
-    msg.innerHTML = 'Please enter all fields';
-
-    // Remove error after 3 seconds
-   setTimeout(() => msg.remove(),1000);
-  } else {
-    // Create new list item with user
-    const li = document.createElement('li');
-
-    // Add text node with input values
-    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
-
-    // Add HTML
-    // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
-
-    // Append to ul
-    userList.appendChild(li);
-
-    // Clear fields
-    nameInput.value = '';
-    emailInput.value = '';
-  }
+  li.className = 'list-group-item';
+  
+  li.appendChild(document.createTextNode(newItem));
+  var deleteBtn = document.createElement('button');
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  deleteBtn.appendChild(document.createTextNode('X'));
+  li.appendChild(deleteBtn);
+  itemList.appendChild(li);
 }
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
